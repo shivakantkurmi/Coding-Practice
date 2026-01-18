75ee8e91abc3ca76e11095d812b31e5495ea948f
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-01-18 11:01:39 +0530

    Implement largestMagicSquare function in C++

---
+++ DSA/Prefix Sum/Leetcode-1895. Largest Magic Square.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int largestMagicSquare(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 1;
+        vector<vector<int>> row(n, vector<int>(m+1, 0));
+        vector<vector<int>> col(n+1, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) {
+                row[i][j+1] = row[i][j] + grid[i][j];
+                col[i+1][j] = col[i][j] + grid[i][j];
+            }
+        for(int k = 2; k <= min(n, m); k++) {
+            for(int i = 0; i + k - 1 < n; i++) {
+                for(int j = 0; j + k - 1 < m; j++) {
+
+                    int target = row[i][j+k] - row[i][j];
+                    bool ok = true;
+
+                    for(int r = i; r < i + k && ok; r++)
+                        if(row[r][j+k] - row[r][j] != target)
+                            ok = false;
+
+                    for(int c = j; c < j + k && ok; c++)
+                        if(col[i+k][c] - col[i][c] != target)
+                            ok = false;
+
+                    int d1 = 0, d2 = 0;
+                    for(int t = 0; t < k; t++) {
+                        d1 += grid[i+t][j+t];
+                        d2 += grid[i+t][j+k-1-t];
+                    }
+
+                    if(ok && d1 == target && d2 == target)
+                        ans = max(ans, k);
+                }
+            }
+        }
+        return ans;
+    }
+};
