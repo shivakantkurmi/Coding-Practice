76949d3c0c7f9ce1225f25b0043a86f4e030d4b3
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-01-24 20:35:10 +0530

    Add iterative solution for Josephus problem
    Added an iterative approach for the Josephus problem as a comment.

--- DSA/Recursion/GFG-Josephus problem.cpp
+++ DSA/Recursion/GFG-Josephus problem.cpp
@@ -7,3 +7,17 @@ public:
         return (josephus(n - 1, k) + k - 1) % n + 1;
     }
 };
+
+
+// ANOTHER WAY BY ITERATION
+/*
+int josephus(int n, int k) {
+    int res = 0;  // 0-based
+    
+    for (int i = 1; i <= n; i++) {
+        res = (res + k) % i;
+    }
+    
+    return res + 1; 
+}
+*/
