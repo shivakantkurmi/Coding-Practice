f06d325a9fa4ee43a97dc813a4d4e61fc3a85ee4
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-01-27 23:35:25 +0530

    Create Leetcode-2520. Count the Digits That Divide a Number.cpp

---
+++ DSA/Basic Maths/Leetcode-2520. Count the Digits That Divide a Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int count=0;
+        int temp=num;
+        while(temp){
+            int rem=temp%10;
+            if(num%rem==0)count++;
+            temp/=10;
+        }
+        return count;
+    }
+};
