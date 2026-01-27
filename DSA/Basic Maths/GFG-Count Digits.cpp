e13b4969ae456c696ae8b50d6c213264281a00ee
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-01-27 23:41:34 +0530

    Update GFG-Count Digits.cpp

--- DSA/Basic Maths/GFG-Count Digits.cpp
+++ DSA/Basic Maths/GFG-Count Digits.cpp
@@ -10,3 +10,16 @@ class Solution {
         return count;
     }
 };
+//TC-O(log10(n)) sc-O(1)
+
+//2nd Way :
+
+class Solution {
+  public:
+    int countDigits(int n) {
+        // code here
+        int cnt=(int)(log10(n)+1);
+        return cnt;
+    }
+};
+//TC-O(1) sc-O(1)
