9ecdc425a4ada280fbc6abc79f921cb538a14bd9
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-02-07 22:59:54 +0530

    Create GFG-Two Sum - Pair with Given Sum.cpp

---
+++ DSA/Hash/GFG-Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++){
+            int need=target-arr[i];
+            if(mp.find(need)!=mp.end()) return true;
+            mp[arr[i]]++;
+        }
+        return false;
+    }
+};
