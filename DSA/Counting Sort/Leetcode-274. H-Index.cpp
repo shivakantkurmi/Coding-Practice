8d51d02176ae2be6c3cbce0403816e36aa250825
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-02-10 23:50:44 +0530

    Update Leetcode-274. H-Index.cpp

--- DSA/Counting Sort/Leetcode-274. H-Index.cpp
+++ DSA/Counting Sort/Leetcode-274. H-Index.cpp
@@ -25,6 +25,33 @@ Space complexity:𝑂(𝑛)
 (Count array)
 */
 
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        
+        vector<int> count(n+1, 0);
+
+        for(int c : citations){
+            if(c >= n) count[n]++;
+            else count[c]++;
+        }
+
+        int total = 0;
+
+        for(int i=n; i>=0; i--){
+            total += count[i];
+            if(total >= i) return i;
+        }
+
+        return 0;
+    }
+};
+
+
+// 2nd way
+
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
