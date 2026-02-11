2af457dd89c298fb1ef2ca630393bb59e8e0fa1d
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-02-11 10:59:31 +0530

    Create GFG-Prime Factorization.cpp

---
+++ DSA/Basic Maths/GFG-Prime Factorization.cpp
@@ -0,0 +1,9 @@
+void printPrimeFactorization(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            cout << i << " ";
+            n /= i;
+        }
+    }
+    if (n > 1) cout << n;
+}
