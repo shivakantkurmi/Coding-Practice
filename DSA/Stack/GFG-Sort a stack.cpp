12953f536c425760be7303671e365d9ffd6c571a
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-02-12 22:42:22 +0530

    Update GFG-Sort a stack.cpp

--- DSA/Stack/GFG-Sort a stack.cpp
+++ DSA/Stack/GFG-Sort a stack.cpp
@@ -11,3 +11,23 @@ class Solution {
         for(int i:temp)st.push(i);
     }
 };
+
+
+// 2nd way using counting sort (tc- O(n) and SC- O(n) )
+class Solution {
+  public:
+    void sortStack(stack<int> &st) {
+        // code here
+        vector<int> temp;
+        while(!st.empty()){
+            temp.push_back(st.top());
+            st.pop();
+        }
+        vector<int> count(1000+1,0);
+        for(int i:temp)count[i]++;
+        for(int i=0;i<1001;i++){
+            while(count[i]--)
+                    st.push(i);
+        }
+    }
+};
