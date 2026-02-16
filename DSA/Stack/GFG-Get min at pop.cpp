17601d5be02e538eedd43e18e0d090cc8cf8708a
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-02-16 22:16:28 +0530

    Create GFG-Get min at pop.cpp

---
+++ DSA/Stack/GFG-Get min at pop.cpp
@@ -0,0 +1,24 @@
+stack<int> _push(int arr[],int n)
+{
+   // your code here
+   stack<int>s;
+   
+   for(int i=0;i<n;i++){
+       if(s.empty()){
+           s.push(arr[i]);
+       }else{
+           s.push(min(arr[i],s.top()));
+       }
+   }
+   return s;
+}
+
+//Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int>s)
+{
+    // your code here
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
