51d26f311e7107c84ca75cffd56e2304f6916f68
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-05-08 23:37:48 +0530

    Create GFG- Quick Sort.cpp

---
+++ DSA/Recursion/GFG- Quick Sort.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[low];
+
+        int i = low - 1;
+        int j = high + 1;
+
+        while(true) {
+
+            do {
+                i++;
+            } while(arr[i] < pivot);
+
+            do {
+                j--;
+            } while(arr[j] > pivot);
+
+            if(i >= j)
+                return j;
+
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    void quickSort(vector<int>& arr, int low, int high) {
+        if(low >= high) return;
+
+        int p = partition(arr, low, high);
+
+        quickSort(arr, low, p);
+        quickSort(arr, p + 1, high);
+    }
+};
