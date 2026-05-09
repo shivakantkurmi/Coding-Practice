58bea025bbc9a1f2e1b388b1db8309c33aca1a52
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-05-09 21:48:12 +0530

    Create GFG-22. Generate Parentheses.cpp

---
+++ DSA/Backtracking/GFG-22. Generate Parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    vector<string> ans;
+    void generate(int n, int open, int close, string s){
+        if(s.length() == 2 * n){
+            ans.push_back(s);
+            return;
+        }
+        if(open < n)
+            generate(n, open + 1, close, s + '(');
+
+        if(close < open) //as you can only add a closing parentheses when there are more open parentheses on left
+            generate(n, open, close + 1, s + ')');
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        generate(n, 0, 0, "");
+        return ans;
+    }
+};
