867d306070db76ed49ddb78c3c6e1e9715cbe182
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-05-11 00:35:19 +0530

    Update GFG-Subset Sums.cpp

--- DSA/Backtracking/GFG-Subset Sums.cpp
+++ DSA/Backtracking/GFG-Subset Sums.cpp
@@ -17,3 +17,25 @@ class Solution {
         
     }
 };
+
+
+
+//2nd way
+class Solution {
+    vector<int> subSum;
+    void solve(int idx, vector<int>&arr, int sum, int n){
+        if(idx==n){
+            subSum.push_back(sum);
+            return;
+        }
+        solve(idx+1,arr,sum+arr[idx],n);
+        solve(idx+1,arr,sum,n);
+    }
+  public:
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        solve(0,arr,0,arr.size());
+        return subSum;
+        
+    }
+};
