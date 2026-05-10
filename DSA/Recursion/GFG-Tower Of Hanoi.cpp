058d079b60bb6127d205a3a3cd43a88714da9bca
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-05-11 00:26:52 +0530

    Create GFG-Tower Of Hanoi.cpp

---
+++ DSA/Recursion/GFG-Tower Of Hanoi.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        // code here
+        if(n==0) return 0;
+        return 2*(towerOfHanoi(n-1,from,to,aux))+1;
+        
+    }
+};
+
+
+class Solution {
+  public:
+    int count=0;
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        if(n==0) return 0;
+        towerOfHanoi(n-1,from,aux,to);
+        count++;
+        towerOfHanoi(n-1,aux,to,from);
+        return count;
+    }
+};
