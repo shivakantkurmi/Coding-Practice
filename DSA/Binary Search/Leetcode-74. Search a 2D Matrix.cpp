bfa8ea38d2b2b9fdaa681359d28dd76228952915
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-05-17 22:29:03 +0530

    Create Leetcode-74. Search a 2D Matrix.cpp

---
+++ DSA/Binary Search/Leetcode-74. Search a 2D Matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low=0, high=n*m-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int r=mid/m,c=mid%m;
+            if(matrix[r][c]==target) return true;
+            else if(matrix[r][c]<target)low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
+};
