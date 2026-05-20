10b53d990795e97e86d929ccc607ddfb42e5cc32
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-05-21 00:24:18 +0530

    Create GFG-Count all possible paths from top left to bottom right.cpp

---
+++ DSA/DP/GFG-Count all possible paths from top left to bottom right.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+class Solution {
+  public:
+    int numberOfPaths(int m, int n) {
+        // code here
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
