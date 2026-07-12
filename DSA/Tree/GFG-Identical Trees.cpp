38d3d657d1668186dc8c4a9cae6d58a369d241c2
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-07-12 23:39:57 +0530

    Create GFG-Identical Trees.cpp

---
+++ DSA/Tree/GFG-Identical Trees.cpp
@@ -0,0 +1,24 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    bool isIdentical(Node* r1, Node* r2) {
+        // code here
+        if(!r1 && !r2) return true;
+        if(!r1 || !r2) return false;
+        if(r1->data != r2->data) return false;
+        return isIdentical(r1->left,r2->left) && isIdentical(r1->right , r2->right);
+    }
+};
