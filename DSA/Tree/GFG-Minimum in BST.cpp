7922d8a6a4c77223ede957bb01adcfdb5d9f15a8
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-07-12 23:41:44 +0530

    Create GFG-Minimum in BST.cpp

---
+++ DSA/Tree/GFG-Minimum in BST.cpp
@@ -0,0 +1,25 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // code here
+        if(!root)return -1;
+        while(root->left)root=root->left;
+        return root->data;
+        
+    }
+};
