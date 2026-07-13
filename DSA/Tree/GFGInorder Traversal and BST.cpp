2edc733a8f67f244d807038278dac2ce588a4bf0
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-07-13 12:28:03 +0530

    Create GFGInorder Traversal and BST.cpp

---
+++ DSA/Tree/GFGInorder Traversal and BST.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool doesRepresentBST(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>=arr[i+1]) return false;
+        }
+        return true;
+    }
+};
