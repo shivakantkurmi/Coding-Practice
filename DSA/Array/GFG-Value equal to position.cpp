54cba2ff1de644f1a73aefb94326481da4639b35
Shivakant Kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2026-07-16 15:13:44 +0530

    Create GFG-Value equal to position.cpp

---
+++ DSA/Array/GFG-Value equal to position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    vector<int> valEqualToPos(vector<int>& arr) {
+        // code here
+        vector<int> ans;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==i+1)ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
